8fd04a46fbabea1204e85ccfbec7eae6bec6784c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-13 06:44:53 -0500

    Programa de polimorfismo con ptrEmpleado

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -13,54 +13,54 @@ int main(void){
     Empleado E;
     Profesor M;
     Secretaria S;
-    Persona* ptrPersona;
+    Empleado* ptrEmpleado;
 
-    //Apuntador Persona a Instancia Persona
-    cout<<"Apuntador Persona a Instancia Persona"<<endl<<endl;
-    ptrPersona = &P;
-    ptrPersona->pideleAlUsuarioTusDatos();
+//    //Apuntador Empleado a Instancia Persona
+//    cout<<"Apuntador Empleado a Instancia Persona"<<endl<<endl;
+//    ptrEmpleado = &P;
+//    ptrEmpleado->pideleAlUsuarioTusDatos();
+//    cout<<endl<<endl;
+//    ptrEmpleado->muestraTusDatos();
+//    cout<<endl<<endl;
+//    system("pause");
+//    system("cls");
+
+//    //Apuntador Empleado a Instancia Alumno
+//    cout<<"Apuntador Empleado a Instancia Alumno"<<endl<<endl;
+//    ptrEmpleado = &A;
+//    ptrEmpleado->pideleAlUsuarioTusDatos();
+//    cout<<endl<<endl;
+//    ptrEmpleado->muestraTusDatos();
+//    cout<<endl<<endl;
+//    system("pause");
+//    system("cls");
+
+    //Apuntador Empleado a Instancia Empleado
+    cout<<"Apuntador Empleado a Instancia Empleado"<<endl<<endl;
+    ptrEmpleado = &E;
+    ptrEmpleado->pideleAlUsuarioTusDatos();
     cout<<endl<<endl;
-    ptrPersona->muestraTusDatos();
+    ptrEmpleado->muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
-    //Apuntador Persona a Instancia Alumno
-    cout<<"Apuntador Persona a Instancia Alumno"<<endl<<endl;
-    ptrPersona = &A;
-    ptrPersona->pideleAlUsuarioTusDatos();
+    //Apuntador Empleado a Instancia Profesor
+    cout<<"Apuntador Empleado a Instancia Profesor"<<endl<<endl;
+    ptrEmpleado = &M;
+    ptrEmpleado->pideleAlUsuarioTusDatos();
     cout<<endl<<endl;
-    ptrPersona->muestraTusDatos();
-    cout<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    //Apuntador Persona a Instancia Empleado
-    cout<<"Apuntador Persona a Instancia Empleado"<<endl<<endl;
-    ptrPersona = &E;
-    ptrPersona->pideleAlUsuarioTusDatos();
-    cout<<endl<<endl;
-    ptrPersona->muestraTusDatos();
+    ptrEmpleado->muestraTusDatos();
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
-    //Apuntador Persona a Instancia Profesor
-    cout<<"Apuntador Persona a Instancia Profesor"<<endl<<endl;
-    ptrPersona = &M;
-    ptrPersona->pideleAlUsuarioTusDatos();
+    //Apuntador Empleado a Instancia Secretaria
+    cout<<"Apuntador Empleado a Instancia Secretaria"<<endl<<endl;
+    ptrEmpleado = &S;
+    ptrEmpleado->pideleAlUsuarioTusDatos();
     cout<<endl<<endl;
-    ptrPersona->muestraTusDatos();
-    cout<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    //Apuntador Persona a Instancia Secretaria
-    cout<<"Apuntador Persona a Instancia Secretaria"<<endl<<endl;
-    ptrPersona = &S;
-    ptrPersona->pideleAlUsuarioTusDatos();
-    cout<<endl<<endl;
-    ptrPersona->muestraTusDatos();
+    ptrEmpleado->muestraTusDatos();
     cout<<endl<<endl;
 
     return 0;
