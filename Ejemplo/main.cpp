e0af5eac9d2ea7cdb37b5842bd8afb01f31baca4
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-13 07:40:32 -0500

    Sobrecarga de una funcion

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,67 +1,34 @@
 #include <iostream>
-#include <stdlib.h>
-#include "Persona.h"
-#include "Alumno.h"
-#include "Empleado.h"
-#include "Profesor.h"
-#include "Secretaria.h"
 using namespace std;
 
+void suma(void);
+void suma(int a);
+void suma(float a, float b);
+void suma(string a, string b);
+
 int main(void){
-    Persona P;
-    Alumno A;
-    Empleado E;
-    Profesor M;
-    Secretaria S;
-    Empleado* ptrEmpleado;
 
-//    //Apuntador Empleado a Instancia Persona
-//    cout<<"Apuntador Empleado a Instancia Persona"<<endl<<endl;
-//    ptrEmpleado = &P;
-//    ptrEmpleado->pideleAlUsuarioTusDatos();
-//    cout<<endl<<endl;
-//    ptrEmpleado->muestraTusDatos();
-//    cout<<endl<<endl;
-//    system("pause");
-//    system("cls");
-
-//    //Apuntador Empleado a Instancia Alumno
-//    cout<<"Apuntador Empleado a Instancia Alumno"<<endl<<endl;
-//    ptrEmpleado = &A;
-//    ptrEmpleado->pideleAlUsuarioTusDatos();
-//    cout<<endl<<endl;
-//    ptrEmpleado->muestraTusDatos();
-//    cout<<endl<<endl;
-//    system("pause");
-//    system("cls");
-
-    //Apuntador Empleado a Instancia Empleado
-    cout<<"Apuntador Empleado a Instancia Empleado"<<endl<<endl;
-    ptrEmpleado = &E;
-    ptrEmpleado->pideleAlUsuarioTusDatos();
-    cout<<endl<<endl;
-    ptrEmpleado->muestraTusDatos();
-    cout<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    //Apuntador Empleado a Instancia Profesor
-    cout<<"Apuntador Empleado a Instancia Profesor"<<endl<<endl;
-    ptrEmpleado = &M;
-    ptrEmpleado->pideleAlUsuarioTusDatos();
-    cout<<endl<<endl;
-    ptrEmpleado->muestraTusDatos();
-    cout<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    //Apuntador Empleado a Instancia Secretaria
-    cout<<"Apuntador Empleado a Instancia Secretaria"<<endl<<endl;
-    ptrEmpleado = &S;
-    ptrEmpleado->pideleAlUsuarioTusDatos();
-    cout<<endl<<endl;
-    ptrEmpleado->muestraTusDatos();
-    cout<<endl<<endl;
+    suma();
+    suma(2);
+    suma(2,3);
+    suma(2.2,3.3);
+    suma("2","3");
 
     return 0;
+}
+
+void suma(void){
+    cout<<2<<" + "<<3<<" = "<<2+3<<endl;
+}
+
+void suma(int a){
+    cout<<a<<" + "<<3<<" = "<<a+3<<endl;
+}
+
+void suma(float a, float b){
+    cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+}
+
+void suma(string a, string b){
+    cout<<a<<" + "<<b<<" = "<<"5"<<endl;
 }
