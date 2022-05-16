563994002bca051c85739c91c82f684f9de7a4a3
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-16 07:06:58 -0500

    idem

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -3,32 +3,40 @@ using namespace std;
 
 void suma(void);
 void suma(int a);
-void suma(float a, float b);
+void suma(int a, int b);
+void suma(float i, float j);
 void suma(string a, string b);
 
 int main(void){
 
+    int w = 2;
+    int x = 3;
+    float y = 2;
+    float z = 3;
+    string cad1 = "2";
+    string cad2 = "3";
+
     suma();
-    suma(2);
-    suma(2,3);
-    suma(2.2,3.3);
-    suma("2","3");
+    suma(w);
+    suma(w,x);
+    suma(y,z);
+    suma(cad1,cad2);
 
     return 0;
 }
 
 void suma(void){
-    cout<<2<<" + "<<3<<" = "<<2+3<<endl;
+    cout << 2 << " + " << 3 << " = " << 2+3 << endl;
 }
-
 void suma(int a){
-    cout<<a<<" + "<<3<<" = "<<a+3<<endl;
+    cout << a << " + " << 3 << " = " << a+3 << endl;
 }
-
+void suma(int a, int b){
+    cout << a << " + " << b << " = " << a+b << endl;
+}
 void suma(float a, float b){
-    cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+    cout << a << " + " << b << " = " << a+b << endl;
 }
-
 void suma(string a, string b){
-    cout<<a<<" + "<<b<<" = "<<"5"<<endl;
+    cout << a << " + " << b << " = " << "5" << endl;
 }
