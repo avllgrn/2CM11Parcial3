ef9d1ead339d7363f08b6d33f82f6fc1f4eb4165
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-17 07:48:49 -0500

    Programa para probar == y != con puntos

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -4,14 +4,20 @@ using namespace std;
 
 int main(void){
 
-    Punto A, B, C;
+    Punto A, B, C, D;
 
     A.pideleAlUsuarioTusDatos();
     B.pideleAlUsuarioTusDatos();
 
-    C = A+B;
+    if(A==B)
+        cout<<"Estan en la misma posicion =^)"<<endl;
+    else
+        cout<<"NO estan en la misma posicion =^("<<endl;
 
-    C.muestraTusDatos();
+    if(A!=B)
+        cout<<"NO estan en la misma posicion =^("<<endl;
+    else
+        cout<<"Estan en la misma posicion =^)"<<endl;
 
     return 0;
 }
