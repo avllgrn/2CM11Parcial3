c8b7c7d7f5a0bc054be0913ce26e23d10deecb17
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-19 07:31:56 -0500

    Ejemplo de referencias a variables

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -2,13 +2,32 @@
 #include "Punto.h"
 using namespace std;
 
+void funcion(int&a, int& b, int &c);
+
 int main(void){
+    int x,y,z;
 
-    Punto A;
+    cout<<x<<endl
+        <<y<<endl
+        <<z<<endl<<endl;
 
-    cin>>A;
+    funcion(x,y,z);
 
-    cout<<A;
+    cout<<x<<endl
+        <<y<<endl
+        <<z<<endl<<endl;
 
     return 0;
+}
+
+void funcion(int&a, int& b, int &c){
+    cout<<a<<endl
+        <<b<<endl
+        <<c<<endl<<endl;
+
+    a=1;b=2;c=3;
+
+    cout<<a<<endl
+        <<b<<endl
+        <<c<<endl<<endl;
 }
