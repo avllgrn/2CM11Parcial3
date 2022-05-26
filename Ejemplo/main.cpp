88afdbc5dc1d668ca1d02657cf8649d2c0467380
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-26 07:44:45 -0500

    Inicializar, leer y mostrar n elementos de un vector (de hasta 10)

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -4,24 +4,27 @@ using namespace std;
 int main(void){
 
     int V[10];
-    int i;
+    int i,n;
 
-    for(i=0; i<10; i++)
+    cout<<"Ingresa n ";
+    cin>>n;
+
+    for(i=0; i<n; i++)
         cout<<"V["<<i<<"] = "<<V[i]<<endl;
 
-    for(i=0; i<10; i++)
+    for(i=0; i<n; i++)
         V[i] = 0;
 
-    for(i=0; i<10; i++)
+    for(i=0; i<n; i++)
         cout<<"V["<<i<<"] = "<<V[i]<<endl;
 
 
-    for(i=0; i<10; i++){
+    for(i=0; i<n; i++){
         cout<<"Ingresa V["<<i<<"] ";
         cin>>V[i];
     }
 
-    for(i=0; i<10; i++)
+    for(i=0; i<n; i++)
         cout<<"V["<<i<<"] = "<<V[i]<<endl;
 
     return 0;
