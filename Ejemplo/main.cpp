de605cd5347faa6020474558bddf85ff5719bbe6
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-05-26 08:27:23 -0500

    Leer y mostrar un vector dinamico de objetos Punto

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
+#include "Punto.h"
 using namespace std;
 
 int main(void){
-    int* V;
+    Punto* V;
     int i,n;
 
     cout<<"Ingresa n ";
     cin>>n;
 
-    V = new int[n];//Se solicita memoria en tiempo de ejecucion
-
-    for(i=0; i<n; i++)
-        cout<<"V["<<i<<"] = "<<V[i]<<endl;
-
-    for(i=0; i<n; i++)
-        V[i] = 0;
+    V = new Punto[n];//Se solicita memoria en tiempo de ejecucion
 
     for(i=0; i<n; i++)
         cout<<"V["<<i<<"] = "<<V[i]<<endl;
 
     for(i=0; i<n; i++){
-        cout<<"Ingresa V["<<i<<"] ";
+        cout<<"Ingresa V["<<i<<"] "<<endl;
         cin>>V[i];
+        cout<<endl;
     }
 
     for(i=0; i<n; i++)
