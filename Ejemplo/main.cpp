16157000a0851e7cdbc45aa49e9e86ec4cf68cd4
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-06-06 07:35:15 -0500

    4 funciones para cada tipo de dato

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,49 +1,64 @@
 #include <iostream>
-#include <stdlib.h>
-#include "Pila.h"
 using namespace std;
 
+int suma(int x, int y);
+float suma(float x, float y);
+long suma(long x, long y);
+double suma(double x, double y);
+
 int main(void){
-    Pila P;
-    int op,d;
+    int a,b,c;
 
-    do{
-        system("cls");
-        cout<<"1. push"<<endl
-            <<"2. pop"<<endl
-            <<"3. libera Pila"<<endl
-            <<"4. Salir"<<endl
-            <<"Cual es tu opcion? ";
-        cin>>op;
-        system("cls");
-        switch(op){
-            case 1:
-                cout<<"Ingresa dato ";cin>>d;
-                P.push(d);
-                break;
-            case 2:
-                if(P.estaVacia()){
-                    cout<<"La pila esta vacia... =("<<endl;
-                }
-                else{
-                    d = P.pop();
-                    cout<<"Salio nodo con dato "<<d<<endl<<endl;
-                }
-                break;
-            case 3:
-                P.liberaPila();
-                break;
-            case 4:
-                cout<<"Adios! =)"<<endl<<endl;
-                break;
-            default :
-                cout<<"Opcion invalida...! =("<<endl<<endl;
-                break;
-        }
-        if(op!=4)
-            system("pause");
+    cout<<"Igresa a ";cin>>a;
+    cout<<"Igresa b ";cin>>b;
+    c=suma(a,b);
+    cout<<a<<" + "<<b<<" = "<<c<<endl;
 
-    }while(op!=4);
+    float d,e,f;
+
+    cout<<"Igresa d ";cin>>d;
+    cout<<"Igresa e ";cin>>e;
+    f=suma(d,e);
+    cout<<d<<" + "<<e<<" = "<<f<<endl;
+
+    long g,h,i;
+
+    cout<<"Igresa g ";cin>>g;
+    cout<<"Igresa h ";cin>>h;
+    i=suma(g,h);
+    cout<<g<<" + "<<h<<" = "<<i<<endl;
+
+    double j,k,l;
+
+    cout<<"Igresa j ";cin>>j;
+    cout<<"Igresa k ";cin>>k;
+    l=suma(j,k);
+    cout<<j<<" + "<<k<<" = "<<l<<endl;
 
     return 0;
+}
+
+int suma(int x, int y){
+    int z;
+    cout<<"suma "<<"int"<<endl;
+    z = x+y;
+    return z;
+}
+float suma(float x, float y){
+    float z;
+    cout<<"suma "<<"float"<<endl;
+    z = x+y;
+    return z;
+}
+long suma(long x, long y){
+    float z;
+    cout<<"suma "<<"long"<<endl;
+    z = x+y;
+    return z;
+}
+double suma(double x, double y){
+    double z;
+    cout<<"suma "<<"double"<<endl;
+    z = x+y;
+    return z;
 }
