7a1bb0938e68836b8a118f0c9aea992665b2e530
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-06-07 08:13:22 -0500

    Pruebas de TNodo<Fraccion>

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include "TNodo.h"
+#include "Fraccion.h"
 using namespace std;
 
 int main(void){
-    TNodo<char>  A;
-    TNodo<char>  B('@',&A);
-    TNodo<char>  C('#',&B);
-    TNodo<char>  D('&',&C);
-    TNodo<char>* tope=&D;
+    Fraccion Temporal;
+    TNodo<Fraccion>  A;
+    Temporal.modificaTusDatos(1,2);
+    TNodo<Fraccion>  B(Temporal,&A);
+    Temporal.modificaTusDatos(3,4);
+    TNodo<Fraccion>  C(Temporal,&B);
+    Temporal.modificaTusDatos(5.6,7.8);
+    TNodo<Fraccion>  D(Temporal,&C);
+    TNodo<Fraccion>* tope=&D;
 
     cout<<"&A   = "<<&A<<endl
         <<"&B   = "<<&B<<endl
