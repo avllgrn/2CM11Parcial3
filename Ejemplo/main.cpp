ae047c294eb35474c36d013b8e2cd267d263bc42
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-06-09 07:34:09 -0500

    Pila manualmente construido, con Objeto tipo Pila (char)

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -1,57 +1,26 @@
 #include <iostream>
 #include <stdlib.h>
-#include "TNodo.h"
+#include "TPila.h"
 using namespace std;
 
 int main(void){
-    TNodo<float>* tope;
-    TNodo<float>* aux;
-    float d;
-
-    tope = NULL;
-
-    if(tope==NULL)
+    TPila<char> P;
+    if(P.estaVacia())
         cout<<"Pila vacia"<<endl;
 
-    tope = new TNodo<float>(1.2,tope);system("pause");
-    tope = new TNodo<float>(3.4,tope);system("pause");
-    tope = new TNodo<float>(5.6,tope);system("pause");
-    tope = new TNodo<float>(7.8,tope);system("pause");
+    P.push('@');system("pause");
+    P.push('#');system("pause");
+    P.push('&');system("pause");
+    P.push('?');system("pause");
 
-    d = tope->dameTuDato();
-    aux = tope;
-    tope = tope->dameTuInferior();
-    delete aux;
-    cout<<d<<endl;system("pause");
+    cout<<P.pop()<<endl;system("pause");
+    cout<<P.pop()<<endl;system("pause");
+    P.push('{');system("pause");
+    cout<<P.pop()<<endl;system("pause");
+    cout<<P.pop()<<endl;system("pause");
+    cout<<P.pop()<<endl;system("pause");
 
-    d = tope->dameTuDato();
-    aux = tope;
-    tope = tope->dameTuInferior();
-    delete aux;
-    cout<<d<<endl;system("pause");
-
-
-    tope = new TNodo<float>(9.8,tope);system("pause");
-
-    d = tope->dameTuDato();
-    aux = tope;
-    tope = tope->dameTuInferior();
-    delete aux;
-    cout<<d<<endl;system("pause");
-
-    d = tope->dameTuDato();
-    aux = tope;
-    tope = tope->dameTuInferior();
-    delete aux;
-    cout<<d<<endl;system("pause");
-
-    d = tope->dameTuDato();
-    aux = tope;
-    tope = tope->dameTuInferior();
-    delete aux;
-    cout<<d<<endl;system("pause");
-
-    if(tope==NULL)
+    if(P.estaVacia())
         cout<<"Pila vacia"<<endl;
 
     return 0;
